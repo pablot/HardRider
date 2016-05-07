161ebef37928c5a02aa2b17db36b9699d0754263
Paweł Tymura <paweltymura@op.pl>
2016-05-07 16:19:08 +0200

    Added sand for backgroung, changed position of ship to bottom

--- HardRider/GameScene.swift
+++ HardRider/GameScene.swift
@@ -22,20 +22,13 @@ class GameScene: SKScene {
     override func didMoveToView(view:SKView) {
         self.backgroundColor = UIColor.yellowColor()
         
-        spaceShip.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        sand.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        sand.size = CGSize(width: 1024, height: 768)
+        self.addChild(sand)
+        spaceShip.position = CGPoint(x:CGRectGetMidX(self.frame), y:80)
         spaceShip.size = CGSize(width: 100.0, height: 150.0)
         self.addChild(spaceShip)
-        sand.position = CGPoint(x:300, y:0)
-        self.addChild(sand)
-        text.position = CGPoint(x: 500, y: 0)
-        text.fontColor = UIColor.blackColor()
-        text.fontSize = 20
-        self.addChild(text)
-        text2.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        text2.fontColor = UIColor.blackColor()
-        text2.fontSize = 20
-        self.addChild(text2)
-        
+       
         if motionManager.accelerometerAvailable == true {
             motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler:{
                 data, error in
@@ -55,10 +48,7 @@ class GameScene: SKScene {
     }
     
     override func update(currentTime: CFTimeInterval) {
-        //let action = SKAction.moveToX(destX, duration: 1)
-        //self.spaceShip.runAction(action)
-        sand.position = CGPoint(x: sand.position.x + 1, y: sand.position.y + 1)
-        text.text = "wysokość:  \(self.frame.height); szerokość: \(self.frame.width)"
-        text2.text = "\(CGRectGetMidY(self.frame)) ; \(CGRectGetMidX(self.frame))"
+        let action = SKAction.moveToX(destX, duration: 1)
+        self.spaceShip.runAction(action)
     }
 }
